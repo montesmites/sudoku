21f29c44270c87557efc81c035740f2b2c0a66fb
nrknik <nkrnik@montesmites.se>
2018-09-23 08:55:30 +0200

    main method

--- src/main/java/se/montesmites/sudoku/Sudoku.java
+++ src/main/java/se/montesmites/sudoku/Sudoku.java
@@ -1,12 +1,28 @@
 package se.montesmites.sudoku;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Optional;
 
 import static java.util.Map.Entry.comparingByKey;
 import static java.util.function.Function.identity;
 import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toList;
 
 class Sudoku {
+    public static void main(String[] args) throws Exception {
+        var path = Paths.get(args[0]);
+        var lines = Files.lines(path);
+        Sudoku sudoku = new Sudoku();
+        var solution = sudoku.solve(Grid.of(lines.collect(toList())));
+        if (solution.isPresent()) {
+            System.out.println("The solution is:");
+            System.out.println(solution.get().render());
+        } else {
+            System.out.println("There is no solution.");
+        }
+    }
+
     Optional<Grid> solve(Grid grid) {
         return doSolve(1, grid);
     }
